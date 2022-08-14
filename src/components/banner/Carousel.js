9fe5aefd2206099d6e966f062c073aac5e4388b2
Nigam <patelnigam136@gmail.com>
2022-08-14 15:19:01 +0530

    new fetch update

--- src/components/banner/Carousel.js
+++ src/components/banner/Carousel.js
@@ -37,7 +37,7 @@ const Carousel = ({ currency, symbol }) => {
   console.log(trending);
   useEffect(() => {
     fetchTrendingCoins();
-  }, [currency]);
+  }, [currency, fetchTrendingCoins]);
 
   const responsive = {
     0: {
